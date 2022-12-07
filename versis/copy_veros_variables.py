b03164d45fe087ffa31a40039f2137005aed0e97
jpgaertner <jan.gaertner.l@gmail.com>
2022-12-07 12:37:40 +0100

    clean up

--- versis/copy_veros_variables.py
+++ versis/copy_veros_variables.py
@@ -5,6 +5,7 @@ from versis.parameters import heatCapacity, rhoSea, celsius2K, gravity
 
 @veros_routine
 def copy_input(state):
+
     vs = state.variables
 
     # ocean surface velocity, temperature and salinity
@@ -18,22 +19,28 @@ def copy_input(state):
 
     # ocean surface surface total and shortwave heat flux
     vs.Qnet = - vs.forc_temp_surface * heatCapacity * rhoSea
-    vs.Qsw = - vs.SWDown
+    vs.Qsw = - vs.SWdown
 
-    # sea surface height anomaly
-    vs.ssh_an = vs.psi * vs.fCori / gravity
+    # sea surface height anomaly (dividing by the size of the uppermost grid cell
+    # to get the right units)
+    vs.ssh_an = vs.psi[:,:,vs.tau] * vs.fCori / ( gravity * vs.dzw[-1] ) 
 
 
 @veros_routine
 def copy_output(state):
+
     vs = state.variables
 
+    #TODO this is useless as forc_temp_surface gets update in set_forcing_kernel at the beginning
+    # of the time step. This is only useful if versis is executed at the beginning of the time step.
+    # then add also something to update the shortwave flux arriving at the ocean surface
     # set the surface heat flux to the heat flux that is reduced due to a potential ice cover
     vs.forc_temp_surface = - vs.Qnet / ( heatCapacity * rhoSea )
 
+
+    # the surface salt flux vs.forc_salt_surface is set at the end of the growth routine
+
     # update the ocean surface stress by area weighting:
     # wind stress when there is no ice, ice-ocean stress when there is ice
-    vs.surface_taux = state.variables.surface_taux * (1 - state.variables.AreaW) \
-                    + state.variables.fu * state.variables.AreaW
-    vs.surface_tauy = state.variables.surface_tauy * (1 - state.variables.AreaS) \
-                    + state.variables.fv * state.variables.AreaS
+    vs.surface_taux = vs.surface_taux * (1 - vs.AreaW) + vs.fu * vs.AreaW
+    vs.surface_tauy = vs.surface_tauy * (1 - vs.AreaS) + vs.fv * vs.AreaS
