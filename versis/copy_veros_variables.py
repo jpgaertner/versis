ede416abcc13ee1dc3f89c124a37b34d012714f8
jpgaertner <jan.gaertner.l@gmail.com>
2022-12-10 15:06:37 +0100

    comments and move topography copying to setup file

--- versis/copy_veros_variables.py
+++ versis/copy_veros_variables.py
@@ -1,7 +1,7 @@
 from veros import veros_routine
 from veros.core.operators import numpy as npx, update, at
 
-from versis.parameters import heatCapacity, rhoSea, celsius2K, gravity
+from versis.parameters import cpWater, rhoSea, celsius2K, gravity
 
 @veros_routine
 def copy_input(state):
@@ -14,11 +14,8 @@ def copy_input(state):
     vs.theta = vs.temp[:,:,-1,vs.tau] + celsius2K
     vs.ocSalt = vs.salt[:,:,-1,vs.tau]
 
-    # ocean depth (topography)
-    vs.R_low = vs.ht
-
     # ocean surface surface total and shortwave heat flux
-    vs.Qnet = - vs.forc_temp_surface * heatCapacity * rhoSea
+    vs.Qnet = - vs.forc_temp_surface * cpWater * rhoSea
     vs.Qsw = - vs.SWdown
 
     # sea surface height anomaly (dividing by the size of the uppermost grid cell
@@ -31,13 +28,15 @@ def copy_output(state):
 
     vs = state.variables
 
-    # TODO all of this is useless as forc_temp_surface gets updated in set_forcing_kernel at the beginning
-    # of the time step. This is only useful if versis is executed at the beginning of the time step.
-    # then also the shortwave flux arriving at the ocean surface should be updated accordingly
+    # TODO all of this is useless as forc_temp_surface and surface_taux/y gets updated in
+    # set_forcing_kernel at the beginning of the time step. This routine is only useful
+    # if it is executed at the beginning of the time step (before set_forcing_kernel).
 
     # set the surface heat flux to the heat flux that is reduced due to a potential ice cover
-    vs.forc_temp_surface = - vs.Qnet / ( heatCapacity * rhoSea )
+    vs.forc_temp_surface = - vs.Qnet / ( cpWater * rhoSea )
 
+    # all of the surface heat forcing for veros is contained in forc_temp_surface, therefore
+    # no shortwave radiation has to be updated here
 
     # the surface salt flux vs.forc_salt_surface is set at the end of the growth routine
 
